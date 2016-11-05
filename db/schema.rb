74985e515feb61b59a03fc8a57a6759a85a6e0dc
thinh0304 <thinh0304@gmail.com>
2016-11-05 15:09:39 -0700

    Add dropdown for faculty

--- db/schema.rb
+++ db/schema.rb
@@ -37,4 +37,12 @@ ActiveRecord::Schema.define(version: 20161105011846) do
     t.boolean  "admin",           default: false
   end
 
+  create_table "widgets", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "stock"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
 end
