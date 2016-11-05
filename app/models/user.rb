3cf2b190969028d134d5116854eef68e5af40e96
justachoob <justachoob@gmail.com>
2016-11-05 22:52:53 +0000

    fixed max length error in user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
  	EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
 	#PASS_REGEX = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,25}$/
-	PASS_REGEX = /\A(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,20}\Z/i # 6 to 25 characters at least 1 Alphabet and 1 Number:
+	PASS_REGEX = /\A(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,20}\Z/i # 6 to 20 characters at least 1 Alphabet and 1 Number:
 
 	validates :name, :presence=>true, :length => { :within => 6..25}, :uniqueness => true
 
