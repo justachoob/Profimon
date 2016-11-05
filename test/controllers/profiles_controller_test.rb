bab4c671c19675d23021b6bb9c71f806bdf097e0
justachoob <justachoob@gmail.com>
2016-11-05 22:39:50 +0000

    local changes to database yaml plus controller tests commenting for later work

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -6,17 +6,22 @@ class ProfilesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get index" do
-    get profiles_url
+    #gives error no route matches show, profile missing keys id
+    get profile_url, id: @profile
+    #get :show, id: @post
+    #get profile_url
     assert_response :success
   end
 
   test "should get new" do
     get new_profile_url
+    #failure got 302 redirect to login
     assert_response :success
   end
 
   test "should create profile" do
     assert_difference('Profile.count') do
+      #failure expected 3, actual 2
       post profiles_url, params: { profile: { badges: @profile.badges, courses_taken: @profile.courses_taken, current_gpa: @profile.current_gpa, faculty: @profile.faculty, num_of_courses_taken: @profile.num_of_courses_taken, pname: @profile.pname } }
     end
 
@@ -25,20 +30,24 @@ class ProfilesControllerTest < ActionDispatch::IntegrationTest
 
   test "should show profile" do
     get profile_url(@profile)
+    # failure got 302 redirect to login
     assert_response :success
   end
 
   test "should get edit" do
     get edit_profile_url(@profile)
+    #Failure creates a 302 error, redirect to /login
     assert_response :success
   end
 
   test "should update profile" do
+    #failure was redirected to login
     patch profile_url(@profile), params: { profile: { badges: @profile.badges, courses_taken: @profile.courses_taken, current_gpa: @profile.current_gpa, faculty: @profile.faculty, num_of_courses_taken: @profile.num_of_courses_taken, pname: @profile.pname } }
     assert_redirected_to profile_url(@profile)
   end
 
   test "should destroy profile" do
+    #failure didnt change by -1
     assert_difference('Profile.count', -1) do
       delete profile_url(@profile)
     end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,13 +2,20 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get users_new_url
+    get new_user_url
     assert_response :success
   end
 
   test "should get create" do
-    get users_create_url
+    get users_url
     assert_response :success
+    #assert_redirected_to page_url(title: 'login')
   end
+  
+#  test "should get edit" do
+#    get edit_user_url
+#    assert_response :success
+#  end
+  
 
 end
