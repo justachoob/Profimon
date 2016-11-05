2bc5da8fc851651f8bfc52d42046409099f083fb
justachoob <justachoob@gmail.com>
2016-11-05 22:51:15 +0000

    user changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
  	EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
 
 
-	validates :name, :presence=>true, :length => { :within => 6..25}, :uniqueness => true
+	validates :name, :presence=>true, :length => { :within => 6..20}, :uniqueness => true
 
 	validates :email, :presence=>true, :format=>EMAIL_REGEX, :length => {:maximum => 100}, :uniqueness => true
 	validates :password, :confirmation=>true, :length => {:in => 6..20}
