5ec6a9f14c40286c2c0303965aa9355f91532323
justachoob <justachoob@gmail.com>
2016-11-07 09:11:56 +0000

    Functional tests added, Login as method fixed, sessions controller tests added

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -20,10 +20,9 @@ end
 class ActionDispatch::IntegrationTest
 
   # Log in as a particular user.
-  def log_in_as(user, password: 'string123465')
-    post login_path, params: { session: { email: user.email,
-                                          password: password,
-                                          } }
+  def log_in_as(user, password: 'password1')
+    post login_url, params: { session: { name: user.name, password: password} }
+                                          
   end
 end
 
