1536d939092008f7a855fb4b5a7b173db46a68ed
justachoob <justachoob@gmail.com>
2016-11-08 00:21:58 +0000

    added some admin controller tests

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  setup do
+    @user = users(:one)
+  end
+  
+  test "should show user" do
+    log_in_as(@user)
+    get users_url
+    assert_response :success
+  end
+  
+  test "should as for login instead of show user" do
+    #log_in_as(@user)
+    get users_url
+    assert_redirected_to login_url
+  end
 end
