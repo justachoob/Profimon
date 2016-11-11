d2e28dbc1523eea468ed035e2c5130668a9d2f6b
thinh0304 <thinh0304@gmail.com>
2016-11-11 10:34:48 -0800

    add default avatar

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -10,7 +10,7 @@ class Profile < ApplicationRecord
 
   validates :pname, :presence=>true
   validates :faculty, :presence=>true
-	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+	has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/defaultAvatar/default.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 end
