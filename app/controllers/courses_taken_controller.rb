2dc3489e09ddaa2c170393d48c43fc18f3910df5
thinh0304 <thinh0304@gmail.com>
2016-11-19 18:16:41 -0800

    add few lines to course taken index and still not fix the course taken controller

--- app/controllers/courses_taken_controller.rb
+++ app/controllers/courses_taken_controller.rb
@@ -1,9 +1,12 @@
 class CoursesTakenController < ApplicationController
   def show
   end
+
+  def index
+      @courses_taken= Courses_taken.all
+  end
+
   private
-
-
 
     def courses_taken_params
       params.require(:courses_taken).permit(:profile_id, :grade_earned, :course_subject, :course_number, :profimon_name)
