4baf0416912fa1b7e8d642a7712ba0be1345d136
Raymond Sushi <raymondsushi@Raymonds-MacBook-Pro.local>
2016-11-20 04:30:13 -0800

    Bug fix: second dropdown menu for course selection now displays W courses as well

--- db/schema.rb
+++ db/schema.rb
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(version: 20161119231028) do
     t.datetime "updated_at",     null: false
   end
 
+  create_table "people", force: :cascade do |t|
+    t.string   "pname"
+    t.integer  "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "profiles", force: :cascade do |t|
     t.string   "pname"
     t.string   "faculty"
@@ -60,6 +67,38 @@ ActiveRecord::Schema.define(version: 20161119231028) do
     t.float    "gpa_required"
     t.integer  "knowledge"
     t.integer  "homework"
+  end
+
+  create_table "things", force: :cascade do |t|
+    t.string   "tname"
+    t.text     "description"
+    t.integer  "person_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "tokimons", force: :cascade do |t|
+    t.string   "tokiname"
+    t.integer  "weight"
+    t.integer  "height"
+    t.integer  "fly"
+    t.integer  "fight"
+    t.integer  "fire"
+    t.integer  "water"
+    t.integer  "electric"
+    t.integer  "freezing"
+    t.integer  "total"
+    t.integer  "trainer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "trainers", force: :cascade do |t|
+    t.string   "trainername"
+    t.integer  "level"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "team"
   end
 
   create_table "users", force: :cascade do |t|
