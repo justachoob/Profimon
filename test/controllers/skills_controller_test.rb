071501f3bdb1779608748a1dfb9d9cd5294fc483
justachoob <justachoob@gmail.com>
2016-11-21 10:15:27 +0000

    skills models tests

--- test/controllers/skills_controller_test.rb
+++ test/controllers/skills_controller_test.rb
@@ -15,7 +15,7 @@ class SkillsControllerTest < ActionDispatch::IntegrationTest
   test "should get create" do
     log_in_as(users(:one))
     get skills_create_url, params: { skill: {name: "MyString", stamina: 1, subject: "MyString", knowledge: 1, homework: 1 }}
-    assert_redirected_to skills_path
+    assert_response :success
   end
   
   test "should get list of skills" do
