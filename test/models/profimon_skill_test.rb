fc37bd90163eff8ee03d324bfd3b00a4fa847661
justachoob <justachoob@gmail.com>
2016-11-21 10:20:47 +0000

    Profimon skills tests

--- test/models/profimon_skill_test.rb
+++ test/models/profimon_skill_test.rb
@@ -1,7 +1,60 @@
 require 'test_helper'
 
 class ProfimonSkillTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "valid profimon skill name" do 
+  	object = ProfimonSkill.new
+  	object.name = ""
+    object.stamina = 1
+    object.knowledge = 1
+    object.homework = 1
+  	assert_not object.valid?
+  end
+  test "valid profimon skill stamina min" do 
+  	object = ProfimonSkill.new
+  	object.name = "purple"
+    object.stamina = -101
+    object.knowledge = 1
+    object.homework = 1
+  	assert_not object.valid?
+  end
+  test "valid profimon skill sta max" do 
+  	object = ProfimonSkill.new
+  	object.name = "purple"
+    object.stamina = 101
+    object.knowledge = 1
+    object.homework = 1
+  	assert_not object.valid?
+  end
+  test "valid profimon skill knowledge min" do 
+  	object = ProfimonSkill.new
+  	object.name = "purple"
+    object.stamina = 1
+    object.knowledge = -101
+    object.homework = 1
+  	assert_not object.valid?
+  end
+  test "valid profimon skill know max" do 
+  	object = ProfimonSkill.new
+  	object.name = "purple"
+    object.stamina = 1
+    object.knowledge = 101
+    object.homework = 1
+  	assert_not object.valid?
+  end
+  test "valid profimon skill homework min" do 
+  	object = ProfimonSkill.new
+  	object.name = "purple"
+    object.stamina = 1
+    object.knowledge = 1
+    object.homework = -101
+  	assert_not object.valid?
+  end
+  test "valid profimon skill homework max" do 
+  	object = ProfimonSkill.new
+  	object.name = "purple"
+    object.stamina = 1
+    object.knowledge = 1
+    object.homework = 101
+  	assert_not object.valid?
+  end
 end
