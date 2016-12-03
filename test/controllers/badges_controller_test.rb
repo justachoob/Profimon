6eabf701a3be08c33270ec71f74aaf51684aacbd
justachoob <justachoob@gmail.com>
2016-12-03 09:22:04 +0000

    badges controller tests and warning for unwanted actions

--- test/controllers/badges_controller_test.rb
+++ test/controllers/badges_controller_test.rb
@@ -11,11 +11,46 @@ class BadgesControllerTest < ActionDispatch::IntegrationTest
   end
   
   
-  #GET "/badges/7?current_profile_id=7"
-  #Parameters: {"current_profile_id"=>"7", "id"=>"7"}
-  #rendered show
-  #Completed 200
+  test "should show badges" do
+    log_in_as(users(:one))
+    get "/badges/1?current_profile_id=1", params: { current_profile_id: 1, id: 1}
+    assert_response :success
+  end
+
+  test "should delete badges" do
+    log_in_as(users(:one))
+    get "/badges/1?current_profile_id=1", params: { current_profile_id: 1, id: 1}
+    assert_response :success
+    assert_difference('Badge.count', -1) do
+      
+      delete badge_path(badges(:one))
+    end
+    assert_response :redirect
+  end
   
+  test "should create badges" do
+    log_in_as(users(:one))
+    assert_difference('Badge.count') do
+      post badges_path, params: { current_profile_id: 1, id: 1, course: {profile_id: 1, subject: "CMPT" } }
+    end
+    assert_response :redirect
+  end
   
+
+  #someone needs to look into these!!
+  test "create should require logged-in user for badges" do
+    assert_no_difference('Badge.count') do
+      post badges_path, params: { current_profile_id: 1, id: 1, course: {profile_id: 1, subject: "CMPT" } }
+    end
+    assert_redirected_to login_url
+  end
   
+  test "destroy badges should require logged in user" do
+    #
+    assert_no_difference('Badge.count') do
+      delete badge_path(badges(:one))
+    end
+      
+    assert_redirected_to login_url
+  end
 end
