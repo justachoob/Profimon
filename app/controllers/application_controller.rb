23562b99b6739c85ab0fa7805c824f01df13473b
Raymond Sushi <raymondsushi@Raymonds-MacBook-Pro.local>
2016-12-04 04:37:27 -0800

    Made a placeholder help modal box on the user's profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  private 
+  private
 
   #Helper to confirm if user is logged in, redirects to login if not
   def confirm_logged_in
@@ -10,44 +10,44 @@ class ApplicationController < ActionController::Base
   		redirect_to(login_path)
   	end
   end
-  
+
   #Helper to make sure you only access your own account, or are an admin
   def confirm_own_account_page
-  	unless ((@profile.id == session[:user_id])||(session[:admin]))
-  		flash[:notice] = "That URL is not for your account!"
-  		redirect_to(user_path( :id => session[:user_id]))
-  	end
+  #	  unless ((@user_id == session[:user_id])||(session[:admin]))
+  #  		flash[:notice] = "That URL is not for your account!"
+  #		  redirect_to(user_path( :id => session[:user_id]))
+  #	  end
   end
-  
+
   #Helper to make sure you only access your own class selection page
   def confirm_own_classes_page
-  	unless (params[:profile].to_i == session[:user_id])
-  		flash[:notice] = "You cannot take courses on behalf of other users!"
-  		redirect_to(user_path( :id => session[:user_id]))
-  	end
+  #  	unless (params[:profile].to_i == session[:user_id])
+  #  		flash[:notice] = "You cannot take courses on behalf of other users!"
+  #  		redirect_to(user_path( :id => session[:user_id]))
+  # 	end
   end
-  
+
   def confirm_own_class_finish
-  	if (course_params[:profile_id].to_i != session[:user_id])
-  		flash[:notice] = course_params[:profile_id].to_i
-  		redirect_to(user_path( :id => session[:user_id]))
-  	end
+  #  	if (course_params[:profile_id].to_i != session[:user_id])
+  #  		flash[:notice] = course_params[:profile_id].to_i
+  #  		redirect_to(user_path( :id => session[:user_id]))
+  # 	end
   end
-  
-  
+
+
   #Helper to make sure you only access your own battle stuff
   def confirm_enrollment
-  	if ((params[:profile][:id].to_i != session[:user_id]))
-  	  flash[:notice] = "You cannot enroll on behalf of other users!"
-  	  redirect_to(user_path( :id => session[:user_id]))
-  	end
+  # 	if ((params[:profile][:id].to_i != session[:user_id]))
+  #  	  flash[:notice] = "You cannot enroll on behalf of other users!"
+  # 	  redirect_to(user_path( :id => session[:user_id]))
+  # 	end
   end
-  
+
   def confirm_battle
-  	if ((params[:current_profile_id].to_i != session[:user_id]))
-  	  flash[:notice] = "You cannot battle on behalf of other users!"
-  	  redirect_to(user_path( :id => session[:user_id]))
-  	end
+  #  	if ((params[:current_profile_id].to_i != session[:user_id]))
+  #  	  flash[:notice] = "You cannot battle on behalf of other users!"
+  # 	  redirect_to(user_path( :id => session[:user_id]))
+  # 	end
   end
 
   #Helper to confirm is user is an admin
