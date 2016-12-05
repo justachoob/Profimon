91c724f5c87b747d33361b2d3751df75cff5d49c
Raymond Sushi <raymondsushi@Raymonds-MacBook-Pro.local>
2016-12-05 03:08:07 -0800

    Fixed error where no instructor exists for a course

--- app/controllers/class_battles_controller.rb
+++ app/controllers/class_battles_controller.rb
@@ -18,7 +18,10 @@ class ClassBattlesController < ApplicationController
     @course_info_url = @course_url + "/" + class_sections[0]['text']
     class_info = JSON.parse(open(@course_info_url).read)
 
-    @instructor_name = class_info['instructor'][0]['name']
+    @instructor_name = "Mystery Instructor"
+    if class_info['instructor'] != nil
+      @instructor_name = class_info['instructor'][0]['name']
+    end
     @room_number = "Online"
     if class_info['courseSchedule'][0]['buildingCode'] != nil
       @room_number = class_info['courseSchedule'][0]['buildingCode'] + " " + class_info['courseSchedule'][0]['roomNumber']
